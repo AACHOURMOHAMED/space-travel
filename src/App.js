e75987ce2cff36184bb215c8bdd4cad916d2866e
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-08-29 23:21:57 +0100

    fix header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Rockets from './pages/rockets';
+// eslint-disable-next-line import/extensions
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dragons from './pages/Dragons';
